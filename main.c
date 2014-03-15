d333b6ef7321dfc477e4b91c5ee7fa02973a1c22
Paul Nakata <pmn@github.com>
2014-03-15 14:49:08 -0700

    Starting to figure out the basic kinematics of walking

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <Servo.h>
-
+/* PIN LAYOUT
+   LEG            ELBOW PIN       SHOULDER PIN
+   -------------------------------------------
+   1              30              31
+   2              32              33
+   3              34              35
+   4               8               9
+   5              10              11
+   6              12              13
+*/
 Servo Shoulders[6];
 Servo Elbows[6];
 
 void register_servos() {
-    /* PIN LAYOUT
-      LEG            ELBOW PIN       SHOULDER PIN
-      -------------------------------------------
-      1              30              31
-      2              32              33
-      3              34              35
-      4               8               9
-      5              10              11
-      6              12              13
-  */
+  // Hook up all the legs to their appropriate pins.
 
   Elbows[0].attach(30);
   Shoulders[0].attach(31);
@@ -34,16 +34,50 @@ void register_servos() {
   Shoulders[5].attach(13);
 }
 
+void leg_move_neutral(int leg){
+    Shoulders[leg].write(90);
+    delay(100);
+
+    Elbows[leg].write(55);
+    delay(100);
+}
+
+void left_leg_walk_forward(int leg){
+  // Sweep the specified leg in a forward walking motion.
+  Elbows[leg].write(10);
+  Shoulders[leg].write(120);
+
+  // Delay to let everything catch up
+  delay(600);
+
+  // Leg is forward, lower the arm and move it backwards
+  Elbows[leg].write(60);
+  delay(300);
+  Shoulders[leg].write(40);
+  delay(300);
+}
+
+void right_leg_walk_forward(int leg){
+  // Sweep the specified leg in a forward walking motion.
+  Elbows[leg].write(10);
+  Shoulders[leg].write(180-120);
+
+  // Delay to let everything catch up
+  delay(600);
+
+  // Leg is forward, lower the arm and move it backwards
+  Elbows[leg].write(60);
+  delay(300);
+  Shoulders[leg].write(180-40);
+  delay(300);
+}
+
 void init_position() {
   // Elbows should be moved to position 55
   // Shoulders should be moved to position 90
 
   for (int i=0; i<=5; i+=1){
-    Shoulders[i].write(90);
-    delay(100);
-
-    Elbows[i].write(55);
-    delay(100);
+    leg_move_neutral(i);
   }
 }
 
@@ -55,4 +89,8 @@ void setup() {
 
 void loop() {
   // Put main code here; this loops continuously.
+   // Try moving a leg forward
+  left_leg_walk_forward(0);
+
+  right_leg_walk_forward(3);
 }
